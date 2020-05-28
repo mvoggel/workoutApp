821348ff1bbba0cc309df25507637258f39d0afa
mvoggel <64497380+mvoggel@users.noreply.github.com>
2020-05-28 19:20:09 -0400

    Add files via upload

---
+++ WorkoutApp/WA-JS.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function showInput() {
+        document.getElementById('display').innerHTML =             
+		document.getElementById('bench-weight').value +
+		document.getElementById('Weight').innerHTML +
+		document.getElementById('bench-reps').value +
+		document.getElementById('Reps').innerHTML;
+    }
